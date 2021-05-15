cb1a17f1958183f225f1e63efe4d51a326109b6f
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2021-05-15 17:12:47 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,5 +1,7 @@
 player1_score = 0
 player2_score = 0
+Question_Turn = "Player1";
+Answer_Turn = "Player2";
 
 player1Name = localStorage.getItem("Player1_name")
 player2Name = localStorage.getItem("Player2_name")
@@ -24,4 +26,46 @@ function Send_Numbers() {
     document.getElementById("Ouput").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+
+
+function Check() {
+    answer = document.getElementById("input_check_box").value;
+    console.log(answer);
+
+    if (Actual_Answer == answer) {
+        if (Answer_Turn == "Player1") {
+            player1_score = player1_score + 1;
+            document.getElementById("Player1").innerHTML = player1Name + ":" + player1_score;
+        }
+        else {
+            player2_score = player2_score + 1;
+            document.getElementById("Player2").innerHTML = player2Name + ":" + player2_score;
+        }
+    } else {
+        if (Answer_Turn == "Player1") {
+            player1_score = player1_score - 1;
+            document.getElementById("Player1").innerHTML = player1Name + ":" + player1_score;
+        }
+        else {
+            player2_score = player2_score - 1;
+            document.getElementById("Player2").innerHTML = player2Name + ":" + player2_score;
+        }
+    }
+
+    if (Question_Turn == "Player1") {
+        Question_Turn = "Player2";
+        document.getElementById("Player_Question").innerHTML = "Question_Turn:" + player2Name;
+    }else{
+        Question_Turn = "Player1";
+        document.getElementById("Player_Question").innerHTML = "Question_Turn:" + player1Name;
+    }
+    if (Answer_Turn == "Player1") {
+        Answer_Turn = "Player2";
+        document.getElementById("Player_Answer").innerHTML = "Answer_Turn:" + player2Name;
+    }else{
+        Answer_Turn = "Player1";
+        document.getElementById("Player_Answer").innerHTML = "Answer_Turn:" + player1Name;
+    }
+    document.getElementById("Output").innerHTML="";
 }
